f1051fe1eae14a01c435e03be1b3a66107c7db6f
kealanheena <kealanheena@gmail.com>
2020-03-11 23:03:39 +0000

    Chore: Complete lesson 7 & 8

---
+++ advanced_javascript/08_number_methods/main.js
@@ -0,0 +1,9 @@
+// var x = 123;
+
+// document.getElementById("test").innerHTML = `${x.toString()} <br> ${(123).toString()} <br> ${(100 + 123).toString()}`;
+
+// var x = 9.656;
+
+// document.getElementById("test").innerHTML = `${x.toPrecision()} <br> ${x.toPrecision(2)} <br> ${x.toPrecision(4)} <br>${x.toPrecision(6)}`;
+
+document.getElementById("test").innerHTML = `${Number(true)} <br> ${Number(true)} <br> ${Number("10")} <br> ${Number("  10")} <br> ${Number(" 10 ")} <br> ${Number("10.33")} <br> ${Number("10,33")} <br> ${Number(" ")} <br> ${Number("learn")}`
